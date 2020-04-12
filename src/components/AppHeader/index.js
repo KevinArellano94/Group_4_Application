1a59ca85c381e7612f51402e5ec339c038fd956b
Kevin Arellano <Kevin.Arellano94@gmail.com>
2020-04-12 14:37:44 -0400

    Initial commit

---
+++ src/components/AppHeader/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../../assets/logo.svg";
+import styles from "./AppHeader.css"; // eslint-disable-line
+
+const AppHeader = props => {
+  return (
+    <header className="app-header">
+      <div className="app-title-wrapper">
+        <div className="app-title-wrapper">
+          <div className="app-left-nav">
+            <img src={logo} className="app-logo" alt="logo" />
+            <div className="app-title-text">
+              <h1 className="app-title">Groupo 4</h1>
+              <p className="app-intro">Applicacion de necesidades</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default AppHeader;
